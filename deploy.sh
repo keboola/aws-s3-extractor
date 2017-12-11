eeafd41ac26c8b3b8db48f12f3a3b0fc80d37381
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-12-11 10:56:04 +0100

    fix: missing env

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm  \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD  \
     quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-aws-s3`
-docker tag keboola/ex-aws-s3:latest ${REPOSITORY}:${TRAVIS_TAG}
-docker tag keboola/ex-aws-s3:latest ${REPOSITORY}:latest
+docker tag $APP_IMAGE:latest ${REPOSITORY}:${TRAVIS_TAG}
+docker tag $APP_IMAGE:latest ${REPOSITORY}:latest
 eval $(docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
     quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-aws-s3)
