c266fed1adf8b3eb711c240a7c322d2e60adeff9
Vaclav Eder <eddycek.ve@gmail.com>
2019-09-18 11:52:32 -0300

    Add new timeout for creating fixtures

--- tests/Keboola/S3ExtractorTest/Functional/FunctionalTestCase.php
+++ tests/Keboola/S3ExtractorTest/Functional/FunctionalTestCase.php
@@ -6,19 +6,21 @@ use Symfony\Component\Process\Process;
 
 class FunctionalTestCase extends \PHPUnit\Framework\TestCase
 {
-    const AWS_S3_BUCKET_ENV = 'AWS_S3_BUCKET';
-    const AWS_S3_ACCESS_KEY_ENV = 'DOWNLOAD_USER_AWS_ACCESS_KEY';
-    const AWS_S3_SECRET_KEY_ENV = 'DOWNLOAD_USER_AWS_SECRET_KEY';
-    const UPDATE_AWS_S3_ACCESS_KEY_ENV = 'UPLOAD_USER_AWS_ACCESS_KEY';
-    const UPDATE_AWS_S3_SECRET_KEY_ENV = 'UPLOAD_USER_AWS_SECRET_KEY';
-    const UPDATE_AWS_S3_BUCKET = 'AWS_S3_BUCKET';
-    const UPDATE_AWS_REGION = 'AWS_REGION';
+    protected const AWS_S3_BUCKET_ENV = 'AWS_S3_BUCKET';
+    protected const AWS_S3_ACCESS_KEY_ENV = 'DOWNLOAD_USER_AWS_ACCESS_KEY';
+    protected const AWS_S3_SECRET_KEY_ENV = 'DOWNLOAD_USER_AWS_SECRET_KEY';
+    protected const UPDATE_AWS_S3_ACCESS_KEY_ENV = 'UPLOAD_USER_AWS_ACCESS_KEY';
+    protected const UPDATE_AWS_S3_SECRET_KEY_ENV = 'UPLOAD_USER_AWS_SECRET_KEY';
+    protected const UPDATE_AWS_S3_BUCKET = 'AWS_S3_BUCKET';
+    protected const UPDATE_AWS_REGION = 'AWS_REGION';
     protected $path;
 
     public static function setUpBeforeClass()
     {
         parent::setUpBeforeClass();
-        (new Process('php ' . __DIR__ . '/../../../loadS3.php'))->mustRun();
+        (new Process('php ' . __DIR__ . '/../../../loadS3.php'))
+            ->setTimeout(1000)
+            ->mustRun();
     }
 
     public function setUp()
