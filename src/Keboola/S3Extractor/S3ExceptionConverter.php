70293639461933a34a0d88dd69d3670a032874db
Vaclav Eder <eddycek.ve@gmail.com>
2019-09-21 15:30:20 -0300

    Refactoring S3 exception converter

--- src/Keboola/S3Extractor/S3ExceptionConverter.php
+++ src/Keboola/S3Extractor/S3ExceptionConverter.php
@@ -14,32 +14,20 @@ class S3ExceptionConverter
      */
     public static function resolve(S3Exception $e): void
     {
-        self::handleInvalidCredentials($e);
-        self::handleSlowDown($e);
-        self::handleBaseUserErrors($e);
-
-        throw $e;
-    }
-
-    /**
-     * @param S3Exception $e
-     * @throws Exception
-     */
-    private static function handleInvalidCredentials(S3Exception $e): void
-    {
-        if ($e->getStatusCode() === 403) {
-            throw new Exception('Invalid credentials or permissions.', $e->getCode(), $e);
-        }
-    }
-
-    /**
-     * @param S3Exception $e
-     * @throws Exception
-     */
-    private static function handleSlowDown(S3Exception $e): void
-    {
-        if ($e->getStatusCode() === 503) {
-            throw new Exception('Error 503 Slow Down: The number of requests to the S3 bucket is very high, please check your bucket limit.', $e->getCode(), $e);
+        switch ($e->getStatusCode()) {
+            case 403:
+                throw new Exception('Invalid credentials or permissions.', $e->getCode(), $e);
+                break;
+            case 503:
+                throw new Exception('Error 503 Slow Down: The number of requests to the S3 bucket is very high, please check your bucket limit.', $e->getCode(), $e);
+                break;
+            case 400:
+            case 401:
+            case 404:
+                self::handleBaseUserErrors($e);
+                break;
+            default:
+                throw $e;
         }
     }
 
@@ -49,10 +37,6 @@ class S3ExceptionConverter
      */
     private static function handleBaseUserErrors(S3Exception $e): void
     {
-        if (false === in_array($e->getStatusCode(), [400, 401, 404], true)) {
-            return;
-        }
-
         if ($e->getPrevious() && get_class($e->getPrevious()) === ClientException::class) {
             /** @var ClientException $previous */
             $previous = $e->getPrevious();
@@ -69,7 +53,6 @@ class S3ExceptionConverter
             }
             throw new Exception($previous->getMessage());
         }
-
         throw new Exception($e->getMessage());
     }
 }
