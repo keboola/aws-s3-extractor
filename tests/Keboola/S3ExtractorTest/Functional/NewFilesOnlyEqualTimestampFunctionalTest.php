1fe8eb3ce7e0f6d1319530a8324af682f6d924a4
Vaclav Eder <eddycek.ve@gmail.com>
2019-09-25 18:26:24 -0300

    Update new files only equal timestamp test

--- tests/Keboola/S3ExtractorTest/Functional/NewFilesOnlyEqualTimestampFunctionalTest.php
+++ tests/Keboola/S3ExtractorTest/Functional/NewFilesOnlyEqualTimestampFunctionalTest.php
@@ -2,6 +2,8 @@
 
 namespace Keboola\S3ExtractorTest\Functional;
 
+use Keboola\S3Extractor\Config;
+use Keboola\S3Extractor\ConfigDefinition;
 use Keboola\S3Extractor\Extractor;
 use Monolog\Handler\TestHandler;
 use Monolog\Logger;
@@ -31,16 +33,18 @@ class NewFilesOnlyEqualTimestampFunctionalTest extends FunctionalTestCase
     private function runExtraction(TestHandler $testHandler, $state = [])
     {
         $key = "no-unique-timestamps/*";
-        $extractor = new Extractor(
+        $extractor = new Extractor(new Config(
             [
-                "accessKeyId" => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                "#secretAccessKey" => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                "bucket" => getenv(self::AWS_S3_BUCKET_ENV),
-                "key" => $key,
-                "includeSubfolders" => true,
-                "newFilesOnly" => true,
-                "limit" => 1,
-            ],
+                "parameters" => [
+                    "accessKeyId" => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                    "#secretAccessKey" => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                    "bucket" => getenv(self::AWS_S3_BUCKET_ENV),
+                    "key" => $key,
+                    "includeSubfolders" => true,
+                    "newFilesOnly" => true,
+                    "limit" => 1,
+                ],
+            ], new ConfigDefinition),
             $state,
             (new Logger('test'))->pushHandler($testHandler)
         );
