740f7eb1bcb1d9fee553c472bd50846b7434326a
Vaclav Eder <eddycek.ve@gmail.com>
2019-09-30 14:43:19 -0300

    Refactoring function test case

--- tests/Functional/FunctionalTestCase.php
+++ tests/Functional/FunctionalTestCase.php
@@ -1,57 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Keboola\S3ExtractorTest\Functional;
 
-use Keboola\Component\JsonHelper;
-use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use Keboola\DatadirTests\AbstractDatadirTestCase;
 use Symfony\Component\Process\Process;
 
-class FunctionalTestCase extends \PHPUnit\Framework\TestCase
+class FunctionalTestCase extends AbstractDatadirTestCase
 {
-    const AWS_S3_BUCKET_ENV = 'AWS_S3_BUCKET';
-    const AWS_S3_ACCESS_KEY_ENV = 'DOWNLOAD_USER_AWS_ACCESS_KEY';
-    const AWS_S3_SECRET_KEY_ENV = 'DOWNLOAD_USER_AWS_SECRET_KEY';
-    const UPDATE_AWS_S3_ACCESS_KEY_ENV = 'UPLOAD_USER_AWS_ACCESS_KEY';
-    const UPDATE_AWS_S3_SECRET_KEY_ENV = 'UPLOAD_USER_AWS_SECRET_KEY';
-    const UPDATE_AWS_S3_BUCKET = 'AWS_S3_BUCKET';
-    const UPDATE_AWS_REGION = 'AWS_REGION';
-    protected $path;
+    protected const AWS_S3_BUCKET_ENV = 'AWS_S3_BUCKET';
+    protected const AWS_S3_ACCESS_KEY_ENV = 'DOWNLOAD_USER_AWS_ACCESS_KEY';
+    protected const AWS_S3_SECRET_KEY_ENV = 'DOWNLOAD_USER_AWS_SECRET_KEY';
+    protected const UPDATE_AWS_S3_ACCESS_KEY_ENV = 'UPLOAD_USER_AWS_ACCESS_KEY';
+    protected const UPDATE_AWS_S3_SECRET_KEY_ENV = 'UPLOAD_USER_AWS_SECRET_KEY';
+    protected const UPDATE_AWS_S3_BUCKET = 'AWS_S3_BUCKET';
+    protected const UPDATE_AWS_REGION = 'AWS_REGION';
 
-    public static function setUpBeforeClass()
+    public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
-        (new Process('php ' . __DIR__ . '/../loadS3.php'))
-            ->setTimeout(1000)
-            ->mustRun();
-    }
-
-    public function setUp()
-    {
-        $this->path = '/tmp/aws-s3-extractor/' . uniqid();
-        mkdir($this->path, 0777, true);
-    }
-
-    public function tearDown()
-    {
-        passthru('rm -rf ' . $this->path);
-    }
-
-    protected function writeConfig(array $config): void
-    {
-        JsonHelper::writeFile($this->path . '/config.json', $config);
-    }
-
-    protected function getOutputState(): array
-    {
-        try {
-            return JsonHelper::readFile($this->path . '/out/state.json');
-        } catch (FileNotFoundException $exception) {
-            return [];
-        }
-    }
-
-    protected function syncInputState(): void
-    {
-        JsonHelper::writeFile($this->path . '/in/state.json', $this->getOutputState());
+//        (new Process('php ' . __DIR__ . '/../loadS3.php'))
+//            ->setTimeout(1000)
+//            ->mustRun();
     }
 }
