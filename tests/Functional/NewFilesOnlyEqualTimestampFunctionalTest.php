af3b5e7b52a9f1ef48371d4aa9cd8dacd5866d2d
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-02 14:23:15 -0300

    Fix new files only equal timestamp tests

--- tests/Functional/NewFilesOnlyEqualTimestampFunctionalTest.php
+++ tests/Functional/NewFilesOnlyEqualTimestampFunctionalTest.php
@@ -2,51 +2,90 @@
 
 namespace Keboola\S3ExtractorTest\Functional;
 
+use Keboola\Component\JsonHelper;
+
 class NewFilesOnlyEqualTimestampFunctionalTest extends FunctionalTestCase
 {
+    private const TEST_DIRECTORY = 'download-continuously';
+
     public function testSuccessfulDownloadFromFolderContinuouslyStep1(): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/download-continuously/step-1',
-            self::config(),
-            0
-        );
+        $this->runTestByStep(1, [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+        ]);
     }
 
     public function testSuccessfulDownloadFromFolderContinuouslyStep2(): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/download-continuously/step-2',
-            self::config(),
-            0
-        );
+        $this->runTestByStep(2, [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+            'no-unique-timestamps/folder2/collision/file1.csv',
+        ], [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+        ]);
     }
 
     public function testSuccessfulDownloadFromFolderContinuouslyStep3(): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/download-continuously/step-3',
-            self::config(),
-            0
-        );
+        $this->runTestByStep(3, [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+            'no-unique-timestamps/folder2/collision/file1.csv',
+            'no-unique-timestamps/folder2/file1.csv',
+        ], [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+            'no-unique-timestamps/folder2/collision/file1.csv',
+        ]);
     }
 
     public function testSuccessfulDownloadFromFolderContinuouslyStep4(): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/download-continuously/step-4',
-            self::config(),
-            0
-        );
+        $this->runTestByStep(4, [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+            'no-unique-timestamps/folder2/collision/file1.csv',
+            'no-unique-timestamps/folder2/file1.csv',
+            'no-unique-timestamps/folder2/file2.csv',
+        ], [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+            'no-unique-timestamps/folder2/collision/file1.csv',
+            'no-unique-timestamps/folder2/file1.csv',
+        ]);
     }
 
     public function testSuccessfulDownloadFromFolderContinuouslyStep5(): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/download-continuously/step-5',
-            self::config(),
-            0
-        );
+        $this->runTestByStep(5, [
+            'no-unique-timestamps/folder2/file3/file1.csv',
+        ], [
+            'no-unique-timestamps/folder2/collision-file1.csv',
+            'no-unique-timestamps/folder2/collision/file1.csv',
+            'no-unique-timestamps/folder2/file1.csv',
+            'no-unique-timestamps/folder2/file2.csv',
+        ]);
+    }
+
+    /**
+     * @param int $step
+     * @param array $processedFilesOut
+     * @param array|null $processedFilesIn
+     * @throws JsonHelper\JsonHelperException
+     */
+    private function runTestByStep(int $step, array $processedFilesOut, array $processedFilesIn = null): void
+    {
+        $testDirectory = sprintf('%s/%s/step-%s', __DIR__, self::TEST_DIRECTORY, $step);
+
+        JsonHelper::writeFile(sprintf('/%s/expected/data/out/state.json', $testDirectory), [
+            'lastDownloadedFileTimestamp' => self::s3FileLastModified($processedFilesOut[0]),
+            'processedFilesInLastTimestampSecond' => $processedFilesOut,
+        ]);
+
+        if ($processedFilesIn) {
+            JsonHelper::writeFile(sprintf('/%s/source/data/in/state.json', $testDirectory), [
+                'lastDownloadedFileTimestamp' => self::s3FileLastModified($processedFilesIn[0]),
+                'processedFilesInLastTimestampSecond' => $processedFilesIn,
+            ]);
+        }
+
+        $this->runTestWithCustomConfiguration($testDirectory, self::config(), 0);
     }
 
     /**
