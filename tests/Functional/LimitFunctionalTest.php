e31ac1a6c4ee5c67977f2a70113b03d371849c61
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-02 11:46:38 -0300

    Fix limit tests

--- tests/Functional/LimitFunctionalTest.php
+++ tests/Functional/LimitFunctionalTest.php
@@ -2,9 +2,11 @@
 
 namespace Keboola\S3ExtractorTest\Functional;
 
+use Keboola\Component\JsonHelper;
+
 class LimitFunctionalTest extends FunctionalTestCase
 {
-    public function testLimitReached()
+    public function testLimitReached(): void
     {
         $this->runTestWithCustomConfiguration(
             __DIR__ . '/limit-reached',
@@ -23,27 +25,32 @@ class LimitFunctionalTest extends FunctionalTestCase
         );
     }
 
-    public function testLimitNotExceeded()
+    public function testLimitNotExceeded(): void
     {
         $this->runTestWithCustomConfiguration(
             __DIR__ . '/limit-not-exceeded',
             [
                 'parameters' => [
-                'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                'key' => 'f*',
-                'includeSubfolders' => true,
-                'newFilesOnly' => false,
-                'limit' => 10,
+                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                    'key' => 'f*',
+                    'includeSubfolders' => true,
+                    'newFilesOnly' => false,
+                    'limit' => 10,
                 ],
             ],
             0
         );
     }
 
-    public function testLimitNewFilesOnly()
+    public function testLimitNewFilesOnly(): void
     {
+        JsonHelper::writeFile(__DIR__ . '/limit-new-files-only/expected/data/out/state.json', [
+            'lastDownloadedFileTimestamp' => self::s3FileLastModified('folder1/file1.csv'),
+            'processedFilesInLastTimestampSecond' => ['folder1/file1.csv'],
+        ]);
+
         $this->runTestWithCustomConfiguration(
             __DIR__ . '/limit-new-files-only',
             [
