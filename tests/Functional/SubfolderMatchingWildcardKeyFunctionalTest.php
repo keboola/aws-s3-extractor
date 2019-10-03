2ed708b8e8b6e80c607a177e531ab39a6957276f
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-03 14:52:25 -0300

    Update subfolder matching wildcard key test

--- tests/Functional/SubfolderMatchingWildcardKeyFunctionalTest.php
+++ tests/Functional/SubfolderMatchingWildcardKeyFunctionalTest.php
@@ -2,59 +2,43 @@
 
 namespace Keboola\S3ExtractorTest\Functional;
 
-use Keboola\S3Extractor\Config;
-use Keboola\S3Extractor\ConfigDefinition;
-use Keboola\S3Extractor\Extractor;
-use Monolog\Handler\TestHandler;
-use Monolog\Logger;
-
 class SubfolderMatchingWildcardKeyFunctionalTest extends FunctionalTestCase
 {
-    public function testSuccessfulDownloadFromRoot()
+    public function testSuccessfulDownloadFromRoot(): void
     {
-        $key = "collision*";
-        $testHandler = new TestHandler();
-        $extractor = new Extractor(new Config([
-            "parameters" => [
-                "accessKeyId" => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                "#secretAccessKey" => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                "bucket" => getenv(self::AWS_S3_BUCKET_ENV),
-                "key" => $key,
-                "includeSubfolders" => false,
-                "newFilesOnly" => false,
-                "limit" => 0,
+        $this->runTestWithCustomConfiguration(
+            __DIR__ . '/subfolder-matching-wildcard-key/download-from-root',
+            [
+                'parameters' => [
+                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                    'key' => 'collision*',
+                    'includeSubfolders' => false,
+                    'newFilesOnly' => false,
+                    'limit' => 0,
+                ],
             ],
-        ], new ConfigDefinition), [], (new Logger("test"))->pushHandler($testHandler));
-        $extractor->extract($this->path);
-
-        $this->assertFileExists($this->path . "/collision-file1.csv");
-        $this->assertFileEquals(__DIR__ . "/../../../_data/collision-file1.csv", $this->path . "/collision-file1.csv");
-        $this->assertTrue($testHandler->hasInfo("Downloading file /collision-file1.csv"));
-        $this->assertTrue($testHandler->hasInfo("Downloaded 1 file(s)"));
-        $this->assertCount(2, $testHandler->getRecords());
+            0
+        );
     }
 
-    public function testSuccessfulDownloadFromSubfolder()
+    public function testSuccessfulDownloadFromSubfolder(): void
     {
-        $key = "folder2/collision*";
-        $testHandler = new TestHandler();
-        $extractor = new Extractor(new Config([
-            "parameters" => [
-                "accessKeyId" => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                "#secretAccessKey" => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                "bucket" => getenv(self::AWS_S3_BUCKET_ENV),
-                "key" => $key,
-                "includeSubfolders" => false,
-                "newFilesOnly" => false,
-                "limit" => 0,
+        $this->runTestWithCustomConfiguration(
+            __DIR__ . '/subfolder-matching-wildcard-key/download-from-subfolder',
+            [
+                'parameters' => [
+                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                    'key' => 'folder2/collision*',
+                    'includeSubfolders' => false,
+                    'newFilesOnly' => false,
+                    'limit' => 0,
+                ],
             ],
-        ], new ConfigDefinition), [], (new Logger("test"))->pushHandler($testHandler));
-        $extractor->extract($this->path);
-
-        $this->assertFileExists($this->path . "/collision-file1.csv");
-        $this->assertFileEquals(__DIR__ . "/../../../_data/folder2/collision-file1.csv", $this->path . "/collision-file1.csv");
-        $this->assertTrue($testHandler->hasInfo("Downloading file /folder2/collision-file1.csv"));
-        $this->assertTrue($testHandler->hasInfo("Downloaded 1 file(s)"));
-        $this->assertCount(2, $testHandler->getRecords());
+            0
+        );
     }
 }
