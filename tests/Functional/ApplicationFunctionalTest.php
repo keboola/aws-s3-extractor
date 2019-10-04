dd0ae2ca66513998c07e093c97e0a5d14406f6c0
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-04 12:13:46 -0300

    Update application state file new files only test

--- tests/Functional/ApplicationFunctionalTest.php
+++ tests/Functional/ApplicationFunctionalTest.php
@@ -50,14 +50,19 @@ class ApplicationFunctionalTest extends FunctionalTestCase
 
     public function testApplicationStateFileNewFilesOnly(): void
     {
+        $testDirectory = __DIR__ . '/application/state-file-new-files-only';
+        $file = 'file1.csv';
+        self::writeInStateFile($testDirectory, [$file]);
+        self::writeOutStateFile($testDirectory, [$file]);
+
         $this->runTestWithCustomConfiguration(
-            __DIR__ . '/application/state-file-new-files-only',
+            $testDirectory,
             [
                 'parameters' => [
                     'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
                     '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
                     'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                    'key' => '/file1.csv',
+                    'key' => $file,
                     'newFilesOnly' => true,
                     'limit' => 0,
                 ],
