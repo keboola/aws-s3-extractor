e08dbcb7687529d02b29d3132b9b1bd656862ddf
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-08 16:28:23 -0300

    Update wildcard key test for async download

--- tests/Functional/WildcardKeyFunctionalTest.php
+++ tests/Functional/WildcardKeyFunctionalTest.php
@@ -2,6 +2,8 @@
 
 namespace Keboola\S3ExtractorTest\Functional;
 
+use Keboola\DatadirTests\DatadirTestSpecification;
+
 class WildcardKeyFunctionalTest extends FunctionalTestCase
 {
     /**
@@ -25,7 +27,7 @@ class WildcardKeyFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /file1.csv (97 B)',
+                'Downloading file complete /file1.csv (97 B)',
                 'Downloaded 1 file(s) (97 B)',
             ]),
             null
@@ -53,7 +55,7 @@ class WildcardKeyFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /collision-file1.csv (117 B)',
+                'Downloading file complete /collision-file1.csv (117 B)',
                 'Downloaded 1 file(s) (117 B)',
             ]),
             null
@@ -66,28 +68,37 @@ class WildcardKeyFunctionalTest extends FunctionalTestCase
      */
     public function testSuccessfulDownloadFromFolder(bool $initialForwardSlash): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/wildcard-key/download-from-folder',
-            [
-                'parameters' => [
-                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                    'key' => $initialForwardSlash ? '/folder2/*' : 'folder2/*',
-                    'includeSubfolders' => false,
-                    'newFilesOnly' => false,
-                    'limit' => 0,
-                ],
-            ],
+        $specification = new DatadirTestSpecification(
+            __DIR__ . '/wildcard-key/download-from-folder/source/data',
             0,
-            self::convertToStdout([
-                'Downloading file /folder2/collision-file1.csv (133 B)',
-                'Downloading file /folder2/file1.csv (113 B)',
-                'Downloading file /folder2/file2.csv (113 B)',
-                'Downloaded 3 file(s) (359 B)',
-            ]),
-            null
+            null,
+            null,
+            __DIR__ . '/wildcard-key/download-from-folder/expected/data/out'
         );
+
+        $tempDatadir = $this->getTempDatadir($specification);
+
+        self::writeConfigFile($tempDatadir, [
+            'parameters' => [
+                'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                'key' => $initialForwardSlash ? '/folder2/*' : 'folder2/*',
+                'includeSubfolders' => false,
+                'newFilesOnly' => false,
+                'limit' => 0,
+            ],
+        ]);
+
+        $process = $this->runScript($tempDatadir->getTmpFolder());
+        $this->assertMatchesSpecification($specification, $process, $tempDatadir->getTmpFolder());
+        $this->assertEqualsCanonicalizing(explode(PHP_EOL, $process->getOutput()), [
+            'Downloading file complete /folder2/collision-file1.csv (133 B)',
+            'Downloading file complete /folder2/file1.csv (113 B)',
+            'Downloading file complete /folder2/file2.csv (113 B)',
+            'Downloaded 3 file(s) (359 B)',
+            '',
+        ]);
     }
 
     /**
@@ -111,7 +122,7 @@ class WildcardKeyFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /folder2/file3/file1.csv (125 B)',
+                'Downloading file complete /folder2/file3/file1.csv (125 B)',
                 'Downloaded 1 file(s) (125 B)',
             ]),
             null
