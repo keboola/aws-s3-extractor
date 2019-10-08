38aba660c81540ab6dca44c5d6e3f7c23453dfa5
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-08 15:14:24 -0300

    Update new files test for async download

--- tests/Functional/NewFilesOnlyFunctionalTest.php
+++ tests/Functional/NewFilesOnlyFunctionalTest.php
@@ -25,7 +25,7 @@ class NewFilesOnlyFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /file1.csv (97 B)',
+                'Downloading file complete /file1.csv (97 B)',
                 'Downloaded 1 file(s) (97 B)',
             ]),
             null
@@ -60,7 +60,7 @@ class NewFilesOnlyFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /folder2/file1.csv (113 B)',
+                'Downloading file complete /folder2/file1.csv (113 B)',
                 'Downloaded 1 file(s) (113 B)',
             ]),
             null
