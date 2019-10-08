96991801e17e4cfcb0ba51b336895280641abfa4
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-08 16:24:05 -0300

    Update subfolder test for async download

--- tests/Functional/SubfoldersFunctionalTest.php
+++ tests/Functional/SubfoldersFunctionalTest.php
@@ -2,6 +2,8 @@
 
 namespace Keboola\S3ExtractorTest\Functional;
 
+use Keboola\DatadirTests\DatadirTestSpecification;
+
 class SubfoldersFunctionalTest extends FunctionalTestCase
 {
     /**
@@ -10,32 +12,41 @@ class SubfoldersFunctionalTest extends FunctionalTestCase
      */
     public function testSuccessfulDownloadFromRoot(bool $initialForwardSlash): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/subfolders/download-from-root',
-            [
-                'parameters' => [
-                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                    'key' => $initialForwardSlash ? '/f*' : 'f*',
-                    'includeSubfolders' => true,
-                    'newFilesOnly' => false,
-                    'limit' => 0,
-                ],
-            ],
+        $specification = new DatadirTestSpecification(
+            __DIR__ . '/subfolders/download-from-root/source/data',
             0,
-            self::convertToStdout([
-                'Downloading file /file1.csv (97 B)',
-                'Downloading file /folder1/file1.csv (113 B)',
-                'Downloading file /folder2/collision-file1.csv (133 B)',
-                'Downloading file /folder2/collision/file1.csv (133 B)',
-                'Downloading file /folder2/file1.csv (113 B)',
-                'Downloading file /folder2/file2.csv (113 B)',
-                'Downloading file /folder2/file3/file1.csv (125 B)',
-                'Downloaded 7 file(s) (827 B)',
-            ]),
-            null
+            null,
+            null,
+            __DIR__ . '/subfolders/download-from-root/expected/data/out'
         );
+
+        $tempDatadir = $this->getTempDatadir($specification);
+
+        self::writeConfigFile($tempDatadir, [
+            'parameters' => [
+                'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                'key' => $initialForwardSlash ? '/f*' : 'f*',
+                'includeSubfolders' => true,
+                'newFilesOnly' => false,
+                'limit' => 0,
+            ],
+        ]);
+
+        $process = $this->runScript($tempDatadir->getTmpFolder());
+        $this->assertMatchesSpecification($specification, $process, $tempDatadir->getTmpFolder());
+        $this->assertEqualsCanonicalizing(explode(PHP_EOL, $process->getOutput()), [
+            'Downloading file complete /file1.csv (97 B)',
+            'Downloading file complete /folder1/file1.csv (113 B)',
+            'Downloading file complete /folder2/collision-file1.csv (133 B)',
+            'Downloading file complete /folder2/collision/file1.csv (133 B)',
+            'Downloading file complete /folder2/file1.csv (113 B)',
+            'Downloading file complete /folder2/file2.csv (113 B)',
+            'Downloading file complete /folder2/file3/file1.csv (125 B)',
+            'Downloaded 7 file(s) (827 B)',
+            '',
+        ]);
     }
 
     /**
@@ -44,27 +55,36 @@ class SubfoldersFunctionalTest extends FunctionalTestCase
      */
     public function testSuccessfulCollisionDownloadFromRoot(bool $initialForwardSlash): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/subfolders/collision-download-from-root',
-            [
-                'parameters' => [
-                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                    'key' => $initialForwardSlash ? '/c*' : 'c*',
-                    'includeSubfolders' => true,
-                    'newFilesOnly' => false,
-                    'limit' => 0,
-                ],
-            ],
+        $specification = new DatadirTestSpecification(
+            __DIR__ . '/subfolders/collision-download-from-root/source/data',
             0,
-            self::convertToStdout([
-                'Downloading file /collision-file1.csv (117 B)',
-                'Downloading file /collision/file1.csv (117 B)',
-                'Downloaded 2 file(s) (234 B)',
-            ]),
-            null
+            null,
+            null,
+            __DIR__ . '/subfolders/collision-download-from-root/expected/data/out'
         );
+
+        $tempDatadir = $this->getTempDatadir($specification);
+
+        self::writeConfigFile($tempDatadir, [
+            'parameters' => [
+                'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                'key' => $initialForwardSlash ? '/c*' : 'c*',
+                'includeSubfolders' => true,
+                'newFilesOnly' => false,
+                'limit' => 0,
+            ],
+        ]);
+
+        $process = $this->runScript($tempDatadir->getTmpFolder());
+        $this->assertMatchesSpecification($specification, $process, $tempDatadir->getTmpFolder());
+        $this->assertEqualsCanonicalizing(explode(PHP_EOL, $process->getOutput()), [
+            'Downloading file complete /collision-file1.csv (117 B)',
+            'Downloading file complete /collision/file1.csv (117 B)',
+            'Downloaded 2 file(s) (234 B)',
+            '',
+        ]);
     }
 
     /**
@@ -73,30 +93,39 @@ class SubfoldersFunctionalTest extends FunctionalTestCase
      */
     public function testSuccessfulDownloadFromFolder(bool $initialForwardSlash): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/subfolders/download-from-folder',
-            [
-                'parameters' => [
-                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                    'key' => $initialForwardSlash ? '/folder2/*' : 'folder2/*',
-                    'includeSubfolders' => true,
-                    'newFilesOnly' => false,
-                    'limit' => 0,
-                ],
-            ],
+        $specification = new DatadirTestSpecification(
+            __DIR__ . '/subfolders/download-from-folder/source/data',
             0,
-            self::convertToStdout([
-                'Downloading file /folder2/collision-file1.csv (133 B)',
-                'Downloading file /folder2/collision/file1.csv (133 B)',
-                'Downloading file /folder2/file1.csv (113 B)',
-                'Downloading file /folder2/file2.csv (113 B)',
-                'Downloading file /folder2/file3/file1.csv (125 B)',
-                'Downloaded 5 file(s) (617 B)',
-            ]),
-            null
+            null,
+            null,
+            __DIR__ . '/subfolders/download-from-folder/expected/data/out'
         );
+
+        $tempDatadir = $this->getTempDatadir($specification);
+
+        self::writeConfigFile($tempDatadir, [
+            'parameters' => [
+                'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                'key' => $initialForwardSlash ? '/folder2/*' : 'folder2/*',
+                'includeSubfolders' => true,
+                'newFilesOnly' => false,
+                'limit' => 0,
+            ],
+        ]);
+
+        $process = $this->runScript($tempDatadir->getTmpFolder());
+        $this->assertMatchesSpecification($specification, $process, $tempDatadir->getTmpFolder());
+        $this->assertEqualsCanonicalizing(explode(PHP_EOL, $process->getOutput()), [
+            'Downloading file complete /folder2/collision-file1.csv (133 B)',
+            'Downloading file complete /folder2/collision/file1.csv (133 B)',
+            'Downloading file complete /folder2/file1.csv (113 B)',
+            'Downloading file complete /folder2/file2.csv (113 B)',
+            'Downloading file complete /folder2/file3/file1.csv (125 B)',
+            'Downloaded 5 file(s) (617 B)',
+            '',
+        ]);
     }
 
     /**
@@ -105,27 +134,36 @@ class SubfoldersFunctionalTest extends FunctionalTestCase
      */
     public function testSuccessfulDownloadFromAmbiguousPrefix(bool $initialForwardSlash): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/subfolders/download-from-ambiguous-prefix',
-            [
-                'parameters' => [
-                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                    'key' => $initialForwardSlash ? '/folder2/collision*' : 'folder2/collision*',
-                    'includeSubfolders' => true,
-                    'newFilesOnly' => false,
-                    'limit' => 0,
-                ],
-            ],
+        $specification = new DatadirTestSpecification(
+            __DIR__ . '/subfolders/download-from-ambiguous-prefix/source/data',
             0,
-            self::convertToStdout([
-                'Downloading file /folder2/collision-file1.csv (133 B)',
-                'Downloading file /folder2/collision/file1.csv (133 B)',
-                'Downloaded 2 file(s) (266 B)',
-            ]),
-            null
+            null,
+            null,
+            __DIR__ . '/subfolders/download-from-ambiguous-prefix/expected/data/out'
         );
+
+        $tempDatadir = $this->getTempDatadir($specification);
+
+        self::writeConfigFile($tempDatadir, [
+            'parameters' => [
+                'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                'key' => $initialForwardSlash ? '/folder2/collision*' : 'folder2/collision*',
+                'includeSubfolders' => true,
+                'newFilesOnly' => false,
+                'limit' => 0,
+            ],
+        ]);
+
+        $process = $this->runScript($tempDatadir->getTmpFolder());
+        $this->assertMatchesSpecification($specification, $process, $tempDatadir->getTmpFolder());
+        $this->assertEqualsCanonicalizing(explode(PHP_EOL, $process->getOutput()), [
+            'Downloading file complete /folder2/collision-file1.csv (133 B)',
+            'Downloading file complete /folder2/collision/file1.csv (133 B)',
+            'Downloaded 2 file(s) (266 B)',
+            '',
+        ]);
     }
 
     /**
@@ -149,7 +187,7 @@ class SubfoldersFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /folder2/file3/file1.csv (125 B)',
+                'Downloading file complete /folder2/file3/file1.csv (125 B)',
                 'Downloaded 1 file(s) (125 B)',
             ]),
             null
