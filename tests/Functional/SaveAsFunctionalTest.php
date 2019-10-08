27d8d0e7d0866e569fa8badff0a9172eae39c933
Vaclav Eder <eddycek.ve@gmail.com>
2019-10-08 15:38:38 -0300

    Update save as test for async download

--- tests/Functional/SaveAsFunctionalTest.php
+++ tests/Functional/SaveAsFunctionalTest.php
@@ -2,6 +2,8 @@
 
 namespace Keboola\S3ExtractorTest\Functional;
 
+use Keboola\DatadirTests\DatadirTestSpecification;
+
 class SaveAsFunctionalTest extends FunctionalTestCase
 {
     /**
@@ -26,7 +28,7 @@ class SaveAsFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /file1.csv (97 B)',
+                'Downloading file complete /file1.csv (97 B)',
                 'Downloaded 1 file(s) (97 B)',
             ]),
             null
@@ -55,7 +57,7 @@ class SaveAsFunctionalTest extends FunctionalTestCase
             ],
             0,
             self::convertToStdout([
-                'Downloading file /folder1/file1.csv (113 B)',
+                'Downloading file complete /folder1/file1.csv (113 B)',
                 'Downloaded 1 file(s) (113 B)',
             ]),
             null
@@ -68,28 +70,37 @@ class SaveAsFunctionalTest extends FunctionalTestCase
      */
     public function testSuccessfulDownloadFromNestedFolder(bool $initialForwardSlash): void
     {
-        $this->runTestWithCustomConfiguration(
-            __DIR__ . '/save-as/download-from-nested-folder',
-            [
-                'parameters' => [
-                    'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
-                    '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
-                    'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
-                    'key' => $initialForwardSlash ? '/folder2/f*' : 'folder2/f*',
-                    'includeSubfolders' => false,
-                    'newFilesOnly' => false,
-                    'limit' => 0,
-                    'saveAs' => 'folder',
-                ],
-            ],
+        $specification = new DatadirTestSpecification(
+            __DIR__ . '/save-as/download-from-nested-folder/source/data',
             0,
-            self::convertToStdout([
-                'Downloading file /folder2/file1.csv (113 B)',
-                'Downloading file /folder2/file2.csv (113 B)',
-                'Downloaded 2 file(s) (226 B)',
-            ]),
-            null
+            null,
+            null,
+            __DIR__ . '/save-as/download-from-nested-folder/expected/data/out'
         );
+
+        $tempDatadir = $this->getTempDatadir($specification);
+
+        self::writeConfigFile($tempDatadir, [
+            'parameters' => [
+                'accessKeyId' => getenv(self::AWS_S3_ACCESS_KEY_ENV),
+                '#secretAccessKey' => getenv(self::AWS_S3_SECRET_KEY_ENV),
+                'bucket' => getenv(self::AWS_S3_BUCKET_ENV),
+                'key' => $initialForwardSlash ? '/folder2/f*' : 'folder2/f*',
+                'includeSubfolders' => false,
+                'newFilesOnly' => false,
+                'limit' => 0,
+                'saveAs' => 'folder',
+            ],
+        ]);
+
+        $process = $this->runScript($tempDatadir->getTmpFolder());
+        $this->assertMatchesSpecification($specification, $process, $tempDatadir->getTmpFolder());
+
+        $output = explode(PHP_EOL, $process->getOutput());
+        $this->assertCount(4, $output);
+        $this->assertContains('Downloading file complete /folder2/file1.csv (113 B)', $output);
+        $this->assertContains('Downloading file complete /folder2/file2.csv (113 B)', $output);
+        $this->assertContains('Downloaded 2 file(s) (226 B)', $output);
     }
 
     /**
